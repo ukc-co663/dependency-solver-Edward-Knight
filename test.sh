3d7dca7c619ed3e4a3cd387654d762cb31dbcc64
Edward Knight <edw@rdknig.ht>
2018-02-02 16:20:39 +0000

    Update ulimit command
    Now matches https://github.com/ukc-co663/depsolver/commit/2f2f2e7c4c7fee6d4dd1d028439993bf58520a71

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo Starting tests...
 for test in `ls -d ${TESTS}/*/`;
 do
     echo Running ${test}
-    ( ulimit -t 300 -v 1000000 ; ./solve ${test}repository.json ${test}initial.json ${test}constraints.json > commands.json )
+    ( ulimit -t 300 -m 1000000 ; ./solve ${test}repository.json ${test}initial.json ${test}constraints.json > commands.json )
     ${TESTS}/judge.py ${test}/repository.json ${test}/initial.json commands.json ${test}/constraints.json
     # read -n1 -r -p "Press any key to continue..."  # pause
     echo
